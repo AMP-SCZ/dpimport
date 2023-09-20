aa6984cb5a0f7fb418c2146eaa7c8e7c9055ad91
Billah, Tashrif <35086881+tashrifbillah@users.noreply.github.com>
2023-09-20 11:00:04 -0400

    if a file is not in toc, check if it is in metadata

--- dpimport/database/__init__.py
+++ dpimport/database/__init__.py
@@ -68,8 +68,14 @@ class Database(object):
         :type probe: dict
         '''
         doc = self.db.toc.find_one({
-            'content': probe['content'],
+            'content': probe['content']
         })
+        if not doc:
+            # since it's not in data, check if it's in metadata
+            doc = self.db.metadata.find_one({
+                'content': probe['content']
+            })
+
         if doc:
             return True
         return False
