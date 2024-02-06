3483283206cff574381b5b40bc4886fdb297cd8c
Ivan Trujillo Ayala <ivanrts43@gmail.com>
2024-02-06 13:29:10 -0600

    Updated tests
    * Updated tests

--- tests/test_importer_service.py
+++ tests/test_importer_service.py
@@ -20,7 +20,7 @@ class TestImporter(TestCase):
         with patch.object(
             self.importer, "process_data_file"
         ) as mocked_process_data_file:
-            data_file_path = "study-subject-assessment-day1to4.csv"
+            data_file_path = "study-participant-assessment-day1to4.csv"
 
             self.importer.process_file(data_file_path)
             mocked_process_data_file.assert_called_once()
@@ -34,15 +34,15 @@ class TestImporter(TestCase):
             mocked_process_metadata_file.assert_called_once()
 
     def test_processed_data_to_json(self):
-        data_file_path = "study-subject-assessment-day1to4.csv"
+        data_file_path = "study-participant-assessment-day1to4.csv"
         data_file_extension_to_dict = self.importer.DATAFILE.match(
             data_file_path
         ).groupdict()
         mock_data_file_info = {
-            "path": "study-subject-assessment-day1to4.csv",
+            "path": "study-participant-assessment-day1to4.csv",
             "filetype": "text/csv",
             "encoding": "utf-8",
-            "basename": "study-subject-assessment-day1to4.csv",
+            "basename": "study-participant-assessment-day1to4.csv",
             "dirname": "/path/to/files",
             "mtime": 1234567890.0,
             "size": 1024,
@@ -50,7 +50,7 @@ class TestImporter(TestCase):
             "gid": 1000,
             "mode": 0o644,
         }
-        subject_assessments = [
+        participant_assessments = [
             {
                 "var1": 1,
                 "var2": 2,
@@ -80,13 +80,25 @@ class TestImporter(TestCase):
             "mode": 0o644,
         }
         participants = [
-            {"Subject ID": "YA1", "Active": 1, "Consent": "2022-06-02", "Study": "YA"},
-            {"Subject ID": "YA2", "Active": 1, "Consent": "2022-06-02", "Study": "YA"},
+            {
+                "Subject ID": "YA1",
+                "Active": 1,
+                "Consent": "2022-06-02",
+                "Study": "YA",
+            },
+            {
+                "Subject ID": "YA2",
+                "Active": 1,
+                "Consent": "2022-06-02",
+                "Study": "YA",
+            },
         ]
 
         with patch.object(
             self.importer, "_file_info", return_value=mock_data_file_info
-        ), patch.object(self.importer, "_read_csv", return_value=subject_assessments):
+        ), patch.object(
+            self.importer, "_read_csv", return_value=participant_assessments
+        ):
             self.importer.process_data_file(data_file_path, data_file_extension_to_dict)
         with patch.object(
             self.importer, "_file_info", return_value=mock_file_info
@@ -99,18 +111,18 @@ class TestImporter(TestCase):
 
         assert (
             data_to_json[0]
-            == '{"metadata": {"path": "study-subject-assessment-day1to4.csv", "filetype": "text/csv", "encoding": "utf-8", "basename": "study-subject-assessment-day1to4.csv", "dirname": "/path/to/files", "mtime": 1234567890.0, "size": 1024, "uid": 1000, "gid": 1000, "mode": 420, "role": "data", "study": "study", "subject": "subject", "assessment": "assessment", "units": "day", "start": "1", "end": "4", "extension": ".csv", "time_end": "4"}, "subject_assessments": [{"var1": 1, "var2": 2, "var3": "str", "subject": "subject", "site": "study", "assessment": "assessment"}, {"var1": 2, "var2": 2, "var3": "str", "var4": 5, "var6": 6, "var7": "str2", "subject": "subject", "site": "study", "assessment": "assessment"}]}'
+            == '{"metadata": {"path": "study-participant-assessment-day1to4.csv", "filetype": "text/csv", "encoding": "utf-8", "basename": "study-participant-assessment-day1to4.csv", "dirname": "/path/to/files", "mtime": 1234567890.0, "size": 1024, "uid": 1000, "gid": 1000, "mode": 420, "role": "data", "study": "study", "participant": "participant", "assessment": "assessment", "units": "day", "start": "1", "end": "4", "extension": ".csv", "time_end": "4"}, "participant_assessments": [{"var1": 1, "var2": 2, "var3": "str"}, {"var1": 2, "var2": 2, "var3": "str", "var4": 5, "var6": 6, "var7": "str2"}]}'
         )
         assert (
             data_to_json[1]
-            == '{"metadata": {"filetype": "text/csv", "encoding": "utf-8", "dirname": "/path/to/files", "mtime": 1234567890.0, "size": 1024, "uid": 1000, "gid": 1000, "mode": 420, "role": "metadata", "study": "site", "extension": ".csv"}, "participants": [{"Active": 1, "Consent": "2022-06-02", "subject": "YA1", "study": "YA"}, {"Active": 1, "Consent": "2022-06-02", "subject": "YA2", "study": "YA"}]}'
+            == '{"metadata": {"filetype": "text/csv", "encoding": "utf-8", "dirname": "/path/to/files", "mtime": 1234567890.0, "size": 1024, "uid": 1000, "gid": 1000, "mode": 420, "role": "metadata", "study": "site", "extension": ".csv"}, "participants": [{"Active": 1, "Consent": "2022-06-02", "participant": "YA1", "study": "YA"}, {"Active": 1, "Consent": "2022-06-02", "participant": "YA2", "study": "YA"}]}'
         )
 
-        self.importer.data_file["subject_assessments"] = []
+        self.importer.data_file["participant_assessments"] = []
 
         data_to_json = self.importer.processed_data_to_json()
 
         assert data_to_json == (
             None,
-            '{"metadata": {"filetype": "text/csv", "encoding": "utf-8", "dirname": "/path/to/files", "mtime": 1234567890.0, "size": 1024, "uid": 1000, "gid": 1000, "mode": 420, "role": "metadata", "study": "site", "extension": ".csv"}, "participants": [{"Active": 1, "Consent": "2022-06-02", "subject": "YA1", "study": "YA"}, {"Active": 1, "Consent": "2022-06-02", "subject": "YA2", "study": "YA"}]}',
+            '{"metadata": {"filetype": "text/csv", "encoding": "utf-8", "dirname": "/path/to/files", "mtime": 1234567890.0, "size": 1024, "uid": 1000, "gid": 1000, "mode": 420, "role": "metadata", "study": "site", "extension": ".csv"}, "participants": [{"Active": 1, "Consent": "2022-06-02", "participant": "YA1", "study": "YA"}, {"Active": 1, "Consent": "2022-06-02", "participant": "YA2", "study": "YA"}]}',
         )
